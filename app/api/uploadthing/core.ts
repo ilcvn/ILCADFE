37288f20bd990c0e7d22a4063ace353403668be0
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2025-08-18 21:55:56 +0700

    bug: fixed bug images uploading limit size 4mb

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -8,7 +8,7 @@ const auth = (req: Request) => ({ id: 'fakeId' });
 export const ourFileRouter = {
   // Chỉ cho phép upload 1 hình ảnh
   singleImageUploader: f({
-    image: { maxFileSize: '4MB', maxFileCount: 1 },
+    image: { maxFileSize: '16MB', maxFileCount: 1 },
   })
     .middleware(async ({ req }) => {
       const user = await auth(req);
@@ -22,8 +22,8 @@ export const ourFileRouter = {
 
   // Cho phép upload nhiều file
   fileUploader: f({
-    image: { maxFileSize: '4MB', maxFileCount: 1 },
-    video: { maxFileSize: '16MB' },
+    image: { maxFileSize: '16MB', maxFileCount: 1 },
+    video: { maxFileSize: '1GB' },
     pdf: { maxFileSize: '4MB' },
     text: { maxFileSize: '64KB' },
     'application/msword': { maxFileSize: '4MB' },
